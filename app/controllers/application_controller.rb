37e2cb0beaf73ed1e4d07ca7f613ef1553779f31
aradaki <takukanaspy@gmail.com>
2021-10-17 09:14:44 +0000

    [update]meshiterro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_premitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    post_images_path
+  end
+
+  protected
+
+  def configure_premitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0420ebe1b9657283b12feff6fdf329eb3c140d3bb9b4e10ef0881b353df2d956458c5b313d16bc6846632e3fb7bb5f099cb74c3819ee5b7040beca40a35b2a98'
